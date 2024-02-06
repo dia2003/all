0e62a21e11fa1ae45e11e03f3d3089e6699c3f63
dia2003 <158614017+dia2003@users.noreply.github.com>
2024-02-07 03:49:42 +0530

    Update hamming.c

--- hamming.c
+++ hamming.c
@@ -1,84 +1,80 @@
-#include <math.h>
-#include <stdio.h>
-// Store input bits
-int input[32];
-// Store hamming code
-int code[32];
-int ham_calc(int, int);
-void solve(int input[], int);
-// Function to calculate bit for
-// ith position
-int ham_calc(int position, int c_l)
-{
- int count = 0, i, j;
- i = position - 1;
- // Traverse to store Hamming Code
- while (i < c_l) {
- for (j = i; j < i + position; j++) {
- // If current boit is 1
- if (code[j] == 1)
- count++;
- }
- // Update i
- i = i + 2 * position;
- }
- if (count % 2 == 0)
- return 0;
- else
- return 1;
-}
-// Function to calculate hamming code
-void solve(int input[], int n)
-{
- int i, p_n = 0, c_l, j, k;
- i = 0;
- // Find msg bits having set bit
- // at x'th position of number
- while (n > (int)pow(2, i) - (i + 1)) {
- p_n++;
- i++;
- }
- c_l = p_n + n;
- j = k = 0;
- // Traverse the msgBits
- for (i = 0; i < c_l; i++) {
- // Update the code
- if (i == ((int)pow(2, k) - 1)) {
- code[i] = 0;
- k++;
- }
- // Update the code[i] to the
- // input character at index j
- else {
- code[i] = input[j];
- j++;
- }
- }
- // Traverse and update the
- // hamming code
- for (i = 0; i < p_n; i++) {
- // Find current position
- int position = (int)pow(2, i);
- // Find value at current position
- int value = ham_calc(position, c_l);
- // Update the code
- code[position - 1] = value;
- }
- // Print the Hamming Code
- printf("\nThe generated Code Word is: ");
- for (i = 0; i < c_l; i++) {
- printf("%d", code[i]);
- }
-}
-// Driver Code
-void main()
-{
- // Given input message Bit
- input[0] = 0;
- input[1] = 1;
- input[2] = 1;
- input[3] = 1;
- int N = 4;
- // Function Call
- solve(input, N);
-}
+ #include <stdio.h> 
+#include <math.h> 
+int input[32]; 
+int code[32]; 
+int ham_calc(int,int); 
+void main() 
+{ 
+ int n,i,p_n = 0,c_l,j,k; 
+ printf("Please enter the length of the Data Word: "); 
+ scanf("%d",&n); 
+ printf("Please enter the Data Word:\n"); 
+ for(i=0;i<n;i++) 
+ { 
+  scanf("%d",&input[i]); 
+ } 
+ i=0; 
+ while(n>(int)pow(2,i)-(i+1)) 
+ { 
+  p_n++; 
+  i++; 
+ }   
+ c_l = p_n + n; 
+ j=k=0; 
+ for(i=0;i<c_l;i++) 
+ {   
+  if(i==((int)pow(2,k)-1)) 
+  { 
+   code[i]=0; 
+   k++; 
+  } 
+  else 
+  { 
+   code[i]=input[j]; 
+   j++; 
+  } 
+ } 
+ for(i=0;i<p_n;i++) 
+ { 
+  int position = (int)pow(2,i); 
+  int value = ham_calc(position,c_l); 
+  code[position-1]=value; 
+ } 
+ printf("\nThe calculated Code Word is: "); 
+ for(i=0;i<c_l;i++) 
+  printf("%d",code[i]); 
+ printf("\n"); 
+ printf("Please enter the received Code Word:\n"); 
+ for(i=0;i<c_l;i++) 
+  scanf("%d",&code[i]); 
+ int error_pos = 0; 
+ for(i=0;i<p_n;i++) 
+ { 
+  int position = (int)pow(2,i); 
+  int value = ham_calc(position,c_l); 
+  if(value != 0) 
+   error_pos+=position; 
+ } 
+ if(error_pos == 1) 
+  printf("The received Code Word is correct.\n"); 
+ else 
+  printf("Error at bit position: %d\n",error_pos); 
+} 
+int ham_calc(int position,int c_l) 
+{ 
+ int count=0,i,j; 
+ i=position-1; 
+ while(i<c_l) 
+ { 
+  for(j=i;j<i+position;j++) 
+  { 
+   if(code[j] == 1) 
+    count++; 
+  } 
+  i=i+2*position; 
+ } 
+ if(count%2 == 0) 
+  return 0; 
+ else 
+  return 1; 
+}   
