748ee4d28627c4dfcce9731f2816aefa67ec0e61
dia2003 <158614017+dia2003@users.noreply.github.com>
2024-02-07 03:51:02 +0530

    Update tcpserv.c

--- tcpserv.c
+++ tcpserv.c
@@ -1,74 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<errno.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<netinet/in.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<sys/wait.h>
-#include<signal.h>
-#define MYPORT 6490
-#define BACKLOG 10
-int main(void)
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
 {
-int sockfd,fp,new_fd;
-struct sockaddr_in my_addr,their_addr;
-int sin_size,i=0;
-int yes=1;
-char buf1[20],buf2[20000];
-if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)
-{
-perror("socket");
-exit(1);
-}
-if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1)
-{
-perror("setsockopt");
-exit(1);
-}
-my_addr.sin_family=AF_INET;
-my_addr.sin_port=htons(MYPORT);
-my_addr.sin_addr.s_addr=INADDR_ANY;
-memset(&(my_addr.sin_zero), '\0', 8);
-if(bind(sockfd,(struct sockaddr *)&my_addr, sizeof(struct sockaddr)) ==-1)
-{
-perror("Bind");
-exit(1);
-}
-if(listen(sockfd, BACKLOG) == -1)
-{
-perror("listen");
-exit(1);
-}
-printf("\n SERVER is online! \n SERVER: Waiting for the client........\n");
-sin_size=sizeof(struct sockaddr_in);
-if((new_fd=accept(sockfd,(struct sockaddr *)&their_addr, &sin_size))==-1)
-{
-perror("Accept");
-exit(0);
-}
-printf("\n SERVER: Got connection from %s \n", inet_ntoa(their_addr.sin_addr));
-recv(new_fd,&buf1,sizeof(buf1),0);
-printf("File requested is %s\n", buf1);
-if((fp=open(buf1,O_RDONLY))<0)
-{
-printf("File not found\n");
-strcpy(buf2,"File not found");
-}
-else
-{
-printf("SERVER: %s found and ready to transfer.\n",buf1);
-read(fp,&buf2,20000);
-close(fp);
-}
-send(new_fd,&buf2,sizeof(buf2),0);
-close(new_fd);
-close(sockfd);
-printf("Transfer success \n");
-printf("\n");
-return 0;
+    int welcome, new_soc, fd, n;
+    char buffer[1024], fname[50];
+    struct sockaddr_in addr;
+
+    welcome = socket(PF_INET, SOCK_STREAM, 0);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(7891);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    bind(welcome, (struct sockaddr *) &addr, sizeof(addr));
+    printf("\nServer is Online");
+    listen(welcome, 5);
+    new_soc = accept(welcome, NULL, NULL);
+
+    recv(new_soc, fname, 50, 0);
+    printf("\nRequesting for file: %s\n", fname);
+
+    fd = open(fname, O_RDONLY);
+
+    if (fd < 0)
+        send(new_soc, "\nFile not found\n", 15, 0);
+    else
+        while ((n = read(fd, buffer, sizeof(buffer))) > 0)
+            send(new_soc, buffer, n, 0);
+    printf("\nRequest sent\n");
+    close(fd);
+
+    return 0;
 }
